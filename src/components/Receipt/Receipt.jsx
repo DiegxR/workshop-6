ed03204503ddcdd91a1be68686d85c5f8f7097c9
WilintonAscanio <wilintonascanio12@hotmail.com>
2023-03-23 22:25:05 -0500

    Add receipt structure styles

--- src/components/Receipt/Receipt.jsx
+++ src/components/Receipt/Receipt.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import './receipt.scss'
 
 const Receipt = () => {
   return (
-    <div>Receipt</div>
+    <div className='receipt'>
+      <section className='receipt__info'>
+        <div>
+          <h1>FACTURA<span>3000-0101</span></h1>
+          <img src="https://cdn-icons-png.flaticon.com/128/5787/5787016.png" alt="logo" />
+        </div>
+        <h4>VENTABURGERS</h4>
+        <small>Agustín Codazzi</small>
+
+        <table>
+          <thead>
+            <tr>
+              <th>PRODUCTO</th>
+              <th>CANTIDAD</th>
+              <th>PRECIO</th>
+              <th>TOTAL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Hamburguesita</td>
+              <td>1</td>
+              <td>10 lucas</td>
+              <td>10 Lucas</td>
+            </tr>
+            <tr>
+              <td>Hamburguesita</td>
+              <td>1</td>
+              <td>10 lucas</td>
+              <td>10 Lucas</td>
+            </tr>
+            <tr>
+              <td>Hamburguesa</td>
+              <td>1</td>
+              <td>10 lucas</td>
+              <td>10 Lucas</td>
+            </tr>
+          </tbody>
+        </table>
+        <div>
+          <strong>TOTAL</strong>
+          <small>30 Lucas</small>
+        </div>
+
+        <small><strong>Vendedor:</strong>  Wilinton Ascanio</small>
+
+
+
+
+      </section>
+    </div>
   )
 }
 
